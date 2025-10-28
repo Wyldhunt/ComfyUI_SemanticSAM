d1b9d5087c8fed397f5ab69d16515b41709f43ea
Wyld <Wyldhunt@users.noreply.github.com>
2025-10-28 18:34:46 -0400

    Refactor SemanticSAM node for Transformers backend

--- semantic_sam/__init__.py
+++ semantic_sam/__init__.py
@@ -1,6 +1,21 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 
 from .architectures import build_model
-from .build_semantic_sam import prepare_image, plot_results, build_semantic_sam, SemanticSamAutomaticMaskGenerator, SemanticSAMPredictor, plot_multi_results
+from .build_semantic_sam import (
+    SemanticSamAutomaticMaskGenerator,
+    build_semantic_sam,
+    plot_multi_results,
+    plot_results,
+    prepare_image,
+)
+from .predictor import SemanticSAMPredictor
+
+__all__ = [
+    "build_model",
+    "SemanticSamAutomaticMaskGenerator",
+    "build_semantic_sam",
+    "plot_multi_results",
+    "plot_results",
+    "prepare_image",
+    "SemanticSAMPredictor",
+]
